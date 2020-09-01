18a9fb5914b8e178b6f9effaf7311d4d5562a3ba
marades <jkl9510357@gmail.com>
2020-09-01 16:38:43 +0900

    detect.py 모듈화/ path모듈로 사용할 수 있도록 변경

--- detect_original.py
+++ detect_original.py
@@ -1,3 +1,4 @@
+
 import argparse
 import os
 import platform
@@ -10,9 +11,7 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 
-import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
-from .models.experimental import attempt_load
+from models.experimental import attempt_load
 from utils.datasets import LoadStreams, LoadImages
 from utils.general import (
     check_img_size, non_max_suppression, apply_classifier, scale_coords,
@@ -20,24 +19,21 @@ from utils.general import (
 from utils.torch_utils import select_device, load_classifier, time_synchronized
 
 
-def detect(opt):
-    resultNames = []
-    save_img=False
-    out, source, view_img, save_txt, imgsz = \
-        opt['output'], opt['source'], opt['view-img'], opt['save-txt'], opt['img-size']
+def detect(save_img=False):
+    out, source, weights, view_img, save_txt, imgsz = \
+        opt.output, opt.source, opt.weights, opt.view_img, opt.save_txt, opt.img_size
     webcam = source.isnumeric() or source.startswith('rtsp') or source.startswith('http') or source.endswith('.txt')
 
     # Initialize
     set_logging()
-    device = select_device(opt['device'])
+    device = select_device(opt.device)
     if os.path.exists(out):
         shutil.rmtree(out)  # delete output folder
     os.makedirs(out)  # make new output folder
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = opt['model']
-    # model = attempt_load(weights, map_location=device)  # load FP32 model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
     imgsz = check_img_size(imgsz, s=model.stride.max())  # check img_size
     if half:
         model.half()  # to FP16
@@ -76,10 +72,10 @@ def detect(opt):
 
         # Inference
         t1 = time_synchronized()
-        pred = model(img, augment=opt['augment'])[0]
+        pred = model(img, augment=opt.augment)[0]
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt['conf-thres'], opt['iou-thres'], classes=opt['classes'], agnostic=opt['agnostic-nms'])
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
         t2 = time_synchronized()
 
         # Apply Classifier
@@ -98,13 +94,14 @@ def detect(opt):
             s += '%gx%g ' % img.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if det is not None and len(det):
-                det =  torch.tensor(det)
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += '%g %ss, ' % (n, names[int(c)])  # add to string
-                    resultNames.append(names[int(c)])
+
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
@@ -115,6 +112,7 @@ def detect(opt):
                     if save_img or view_img:  # Add bbox to image
                         label = '%s %.2f' % (names[int(cls)], conf)
                         plot_one_box(xyxy, im0, label=label, color=colors[int(cls)], line_thickness=3)
+
             # Print time (inference + NMS)
             print('%sDone. (%.3fs)' % (s, t2 - t1))
 
@@ -125,7 +123,6 @@ def detect(opt):
                     raise StopIteration
 
             # Save results (image with detections)
-            print(save_path)
             if save_img:
                 if dataset.mode == 'images':
                     cv2.imwrite(save_path, im0)
@@ -144,63 +141,35 @@ def detect(opt):
 
     if save_txt or save_img:
         print('Results saved to %s' % Path(out))
-        if platform.system() == 'Darwin' and not opt['update']:  # MacOS
+        if platform.system() == 'Darwin' and not opt.update:  # MacOS
             os.system('open ' + save_path)
 
     print('Done. (%.3fs)' % (time.time() - t0))
-    return save_path, resultNames
 
 
-# if __name__ == '__main__':
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
-# parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-# parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
-# parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-# parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
-# parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
-# parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-# parser.add_argument('--view-img', action='store_true', help='display results')
-# parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-# parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
-# parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
-# parser.add_argument('--augment', action='store_true', help='augmented inference')
-# parser.add_argument('--update', action='store_true', help='update all models')
-# opt = parser.parse_args()
-# print(opt)
-def run(model, source, view_img=False, save_txt=False, classes=None, agnostic_nms=False, augment=False, update=False):
-    arg = {
-        # "weights" = 'yolov5s.pt'
-        "model" : model
-        , "source" : source
-        , "output" : 'output'
-        , "img-size" : 640
-        , "conf-thres" : 0.4
-        , "iou-thres" : 0.5
-        , "device" : '0'
-        , "view-img" : view_img
-        , "save-txt" : save_txt
-        , "classes" : classes
-        , "agnostic-nms" : agnostic_nms
-        , "augment" : augment
-        , "update" : update
-    }
-    
-    return detect(arg)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='yolov5s.pt', help='model.pt path(s)')
+    parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
+    parser.add_argument('--output', type=str, default='inference/output', help='output folder')  # output folder
+    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--conf-thres', type=float, default=0.4, help='object confidence threshold')
+    parser.add_argument('--iou-thres', type=float, default=0.5, help='IOU threshold for NMS')
+    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+    parser.add_argument('--view-img', action='store_true', help='display results')
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--classes', nargs='+', type=int, help='filter by class: --class 0, or --class 0 2 3')
+    parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
+    parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    opt = parser.parse_args()
+    print(opt)
+    print('\n')
 
-# with torch.no_grad():
-#     if opt.update:  # update all models (to fix SourceChangeWarning)
-#         for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
-#             detect()
-#             strip_optimizer(opt.weights)
-#     else:
-#         detect()
-
-# Namespace(agnostic_nms=False, augment=False, classes=None, 
-#         conf_thres=0.4, device='', fourcc='mp4v', half=False, 
-#         img_size=640, iou_thres=0.5, output='inference/output', 
-#         save_txt=False, source='./inference/images/', view_img=False, 
-#         weights='yolov5s.pt')
-
-# python detect_original.py --source 0fEtgP4d.jpg --weights ./weights/AIMTR_MAKER.pt
-# python detect_original.py --source /home/jkl/gh/cmlr-gateway/lib/yolov5/0fEtgP4d.jpg --weights ./weights/AIMTR_MAKER.pt
+    with torch.no_grad():
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['yolov5s.pt', 'yolov5m.pt', 'yolov5l.pt', 'yolov5x.pt']:
+                detect()
+                strip_optimizer(opt.weights)
+        else:
+            detect()
